dbb6bf29cbeb665b8744df85b872c64e57738a2e
aspirina765 <molinalucas765@gmail.com>
2023-01-14 18:10:27 -0300

    refact: Delete folder

--- kafka-connect-tutorial-sources.sh
+++ kafka-connect-tutorial-sources.sh
@@ -32,11 +32,11 @@ docker run --rm -it -v %cd%:/tutorial --net=host landoop/fast-data-dev:cp3.3.0 b
 docker run --rm -it -v ${PWD}:/tutorial --net=host landoop/fast-data-dev:cp3.3.0 bash
 
 # we launch the kafka connector in standalone mode:
-cd /tutorial/source/demo-1
+cd ./tutorial/source/demo-1
 #cd ./source/demo-1
 
 # create the topic we write to with 3 partitions
-kafka-topics --create --topic demo-1-standalone --partitions 3 --replication-factor 1 --zookeeper 127.0.0.1:2181
+kafka-topics --create --topic demo-1-standalone --partitions 3 --replication-factor 1 --bootstrap-server 127.0.0.1:9092
 # Usage is connect-standalone worker.properties connector1.properties [connector2.properties connector3.properties]
 connect-standalone worker.properties file-stream-demo-standalone.properties
 # write some data to the demo-file.txt !
