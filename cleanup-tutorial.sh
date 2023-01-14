42d7acb0bbd7968948b25d95c1274a63d14d6dd1
aspirina765 <molinalucas765@gmail.com>
2023-01-14 19:28:43 -0300

    chore: Some tool

--- cleanup-tutorial.sh
+++ cleanup-tutorial.sh
@@ -2,10 +2,9 @@
 
 docker-compose down kafka-connector-demo-kafka-cluster-1
 
-docker container ls  | grep 'landoop/fast-data-dev:cp3.3.0' | awk '{print $1}'
+# docker container ls  | grep 'landoop/fast-data-dev:cp3.3.0' | awk '{print $1}'
 
 container_del=$(docker container ls  | grep 'landoop/fast-data-dev:cp3.3.0' | awk '{print $1}')
-
 
 docker stop $container_del
 
